6b79dabf4922e5f90522a995743dc2eb54633853
Brayan <68491457+lokuedo5000@users.noreply.github.com>
2024-11-18 17:45:37 -0500

    Add files via upload

---
+++ playsift/suggested.mjs
@@ -0,0 +1,14 @@
+import path from "path";
+
+import PathUtils from "../../utils/craftshelf-fy/js/pathUtils.mjs";
+const { __dirname } = PathUtils.getFileDetails(import.meta.url);
+
+export default [
+  {
+    name: "help",
+    alias: ["h"],
+    description: "Muestra la lista de comandos disponibles",
+    examples: ["help", "h"],
+    handler: path.join(__dirname, "exec", "help.mjs"),
+  },
+];
